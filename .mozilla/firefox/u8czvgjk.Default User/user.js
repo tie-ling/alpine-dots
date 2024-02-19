b87014f30a109389d2edbe29bf2e5bc8d0bc8055
Your Name <you@example.com>
2024-02-19 10:03:28 +0000

    firefox user js
    Signed-off-by: Your Name <you@example.com>

---
+++ .mozilla/firefox/u8czvgjk.Default User/user.js
@@ -0,0 +1,12 @@
+user_pref("apz.allow_double_tap_zooming", false);
+user_pref("apz.allow_zooming", false);
+user_pref("apz.gtk.touchpad_pinch.enabled", false);
+user_pref("webgl.disable-extensions", true);
+user_pref("webgl.disable-fail-if-major-performance-caveat", true);
+user_pref("webgl.disabled", true);
+user_pref("webgl.min_capability_mode", true);
+user_pref("javascript.enabled", false);
+user_pref("javascript.options.asmjs", false);
+user_pref("javascript.options.wasm", false);
+user_pref("javascript.options.ion", false);
+user_pref("javascript.options.baselinejit", false);
